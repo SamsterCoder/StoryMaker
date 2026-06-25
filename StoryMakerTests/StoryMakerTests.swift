d5f3d4552bff947cad1e25870a3a862e970ba632
SamsterCoder <sammy.helbig@icloud.com>
2026-06-25 17:42:44 +0200

    Start Repo

---
+++ StoryMakerTests/StoryMakerTests.swift
@@ -0,0 +1,19 @@
+//
+//  StoryMakerTests.swift
+//  StoryMakerTests
+//
+//  Created by Sam Helbig on 25.06.26.
+//
+
+import Testing
+@testable import StoryMaker
+
+struct StoryMakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
